ab0d51f5e8e1f32bec54c5707f2f509d15df7798
Jackie Ma <jackie.ma@juwai.com>
2014-12-01 16:41:24 +0800

    Adding git configuration

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ echo "Configure vimrc"
 cp -rf .vimrc ~/.vimrc
 echo "Installing Plugin.."
 vim +BundleInstall +qall
+
+echo "Setup git environment.."
+git config --global user.name 'Jackie Ma'
+git config --global user.email 'jackie.ma@juwai.com'
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.df difftool
 echo "Done."
